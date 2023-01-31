84cbe67e5228964eaeb2729748267a80da15b761
misha <mikhail.dyoshin@gmail.com>
2023-01-31 17:30:27 +0300

    Added a few menus, created a toolbar and placed into it a quit-action.

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QMainWindow, QToolBar
 
 
 class MainWindow(QMainWindow):
@@ -8,15 +9,45 @@ class MainWindow(QMainWindow):
         self.app = app
         self.setWindowTitle("Custom Main Window")
 
+        """ Menu bar and menus """
         # Adding menu bar
         menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("&File")
+
+        # Creating file menu
+        file_menu = menu_bar.addMenu("File")
+
+        # Creating edit menu
+        edit_menu = menu_bar.addMenu("Edit")
+
+        # Creating another bunch of menus
+        menu_bar.addMenu("Windows")
+        menu_bar.addMenu("Settings")
+        menu_bar.addMenu("Help")
 
         # Adding action to the file-menu
         quit_action = file_menu.addAction("Quit")
 
+        # Adding actions to the edit-menu
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+
         # Connecting quit-action to its method
         quit_action.triggered.connect(self.quit_app)
+
+
+        """ Toolbar """
+        # Creating toolbar
+        tool_bar = QToolBar("My main toolbar")
+        # Setting the size of icons in the toolbar
+        tool_bar.setIconSize(QSize(16, 16))
+        # Adding toolbar to the window-object
+        self.addToolBar(tool_bar)
+
+        # Adding the quit action to the toolbar
+        tool_bar.addAction(quit_action)
 
     
     def quit_app(self):
