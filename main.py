aa2443ea1174a61790136a18811e2e69bd83ec29
misha <mikhail.dyoshin@gmail.com>
2023-01-31 13:48:53 +0300

    Wired the slot to the signal from button
    Now when button is pressed the programm prints a message in the console.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 #The sys module is responsible for processing commmand line arguments
 import sys
 
+# The slot: responds when "Press me"-button is clicked
+def button_clicked():
+    print("You clicked the button, didn't you?")
 
 # This element is a wrapper for all application widgets and interections
 app = QApplication(sys.argv)
@@ -15,10 +18,10 @@ window = QMainWindow()
 window.setWindowTitle("First python app with PySide6")
 
 # Creating a button
-button = QPushButton()
+button = QPushButton("Press me")
 
-# Plasing a text into the button-widget
-button.setText("Push me")
+# Wiring the slot to the signal from button
+button.clicked.connect(button_clicked)
 
 # Positioning the button in the center of the main window
 window.setCentralWidget(button)
