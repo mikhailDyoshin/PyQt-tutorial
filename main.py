531d09a2aca755e93ac069e83fc66020227704df
misha <mikhail.dyoshin@gmail.com>
2023-02-01 17:53:44 +0300

    Created a new custom widget to represent how different message-boxes work.
    The app's window has a few buttons that, when they are clicked, would show a message box with certain information and buttons. Now the only one (first) button has this functionality which described the hard way, that is there is another easier way to do this.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Importing the components we need
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication
 
 #The sys module is responsible for processing commmand line arguments
 import sys
-
+""" 
 # Importing the ButtonHolder class
 from button_holder import ButtonHolder
 
@@ -12,13 +12,16 @@ from rock_widget import RockWidget
 
 # Importing the MainWindow class
 from main_window import MainWindow
+"""
+
+from widget import Widget
 
 
 # This element is a wrapper for all application widgets and interections
 app = QApplication(sys.argv)
 
 # Creating the main window of the application
-window = MainWindow(app)
+window = Widget()
 
 # Showing the window because it's hidden by default
 window.show()
---
+++ widget.py
@@ -0,0 +1,89 @@
+from PySide6.QtWidgets import QWidget, QMainWindow, QPushButton, QVBoxLayout, QMessageBox
+
+
+class Widget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("QMessageBox")
+
+        """ Buttons """
+        button_hard = QPushButton("Hard")
+        button_hard.clicked.connect(self.button_clicked_hard)
+
+        button_critical = QPushButton("Critical")
+        button_critical.clicked.connect(self.button_clicked_critical)
+
+        button_question = QPushButton("Question")
+        button_question.clicked.connect(self.button_clicked_question)
+
+        button_information = QPushButton("Information")
+        button_information.clicked.connect(self.button_clicked_information)
+
+        button_warning = QPushButton("Warning")
+        button_warning.clicked.connect(self.button_clicked_warning)
+
+        button_about = QPushButton("About")
+        button_about.clicked.connect(self.button_clicked_about)
+
+        """ Layout """
+        # Creating a layout
+        layout = QVBoxLayout()
+
+        # Adding widgets to the layout
+        layout.addWidget(button_hard)
+        layout.addWidget(button_critical)
+        layout.addWidget(button_question)
+        layout.addWidget(button_information)
+        layout.addWidget(button_warning)
+        layout.addWidget(button_about)
+
+        # Setting the layout
+        self.setLayout(layout)
+
+
+    """ Methods """
+    # This is a hard way to represent a message box
+    def button_clicked_hard(self):
+
+        """
+            The method shows a message box 
+            with title, text, informative text, 
+            an icon and standard buttons.
+            Also it can check 
+            which one of the default buttons was clicked.
+        """
+
+        message = QMessageBox()
+        message.setMinimumSize(700, 200)
+        message.setWindowTitle("Some title")
+        message.setText("Something happend")
+        message.setInformativeText("Do you want to do something?")
+        message.setIcon(QMessageBox.Critical)
+        message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        message.setDefaultButton(QMessageBox.Ok)
+
+        # Showing the message-box
+        ret = message.exec()
+
+        # Checking which one of the default buttons was clicked.
+        if ret == QMessageBox.Ok:
+            print("Ok")
+        else:
+            print("Cancel")
+
+    def button_clicked_critical(self):
+        pass
+
+    def button_clicked_question(self):
+        pass
+
+    def button_clicked_information(self):
+        pass
+
+    def button_clicked_warning(self):
+        pass
+
+    def button_clicked_about(self):
+        pass
